b32935000e7bc1e0d7bae186c980453b7e8ae929
Júnior <juniorsbaum@hotmail.com>
2022-07-05 01:03:49 -0300

    added updateUser, deleteUser and addOneUser

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 const url = "http://localhost:5500/api";
+const number = 15
 const newUser =  {
     name: "Matheus Nunes",
     avatar: "https://avatars.githubusercontent.com/u/94872601?v=4",
+    city: "Porto Alegre"
+}
+
+const userUpdated =  {
+    name: "Pedro Peral",
+    avatar: "https://avatars.githubusercontent.com/u/48099618?v=4",
     city: "Porto Alegre"
 }
 
@@ -17,10 +24,36 @@ getUser()
 
 function addNewUser() {
     axios.post(url, newUser)
-    .then(response => {
-        console.log(response.data)
-    })
-    .catch(error => console.log(error))
+        .then(response => {
+            alert(JSON.stringify(response.data))
+        })
+        .catch(error => console.log(error))
 }
+//addNewUser()
 
-addNewUser()
+function updateUser() {
+    axios.put(`${url}/3`, userUpdated)
+        .then(response => {
+            alert(JSON.stringify(response.data))
+        })
+        .catch(error => console.log(error))
+}
+//updateUser()
+
+function deleteUser(){
+    axios.delete(`${url}/10`)
+        .then(response => {
+            alert(JSON.stringify(response.data))
+        })
+        .catch(error => console.log(error))
+}
+//deleteUser()
+
+function getOneUser() {
+    axios.get(`${url}/${number}`)
+    .then(response => {
+        const data = response.data
+        renderResults.textContent = JSON.stringify(data)
+    })
+}
+getOneUser()
