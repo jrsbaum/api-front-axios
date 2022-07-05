3b72866f802ba1ca02e8b319123de463dfce3efd
Júnior <juniorsbaum@hotmail.com>
2022-07-05 00:05:23 -0300

    add api consume functions

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-const url="http://localhost:5500/api";
+const url = "http://localhost:5500/api";
+const newUser =  {
+    name: "Matheus Nunes",
+    avatar: "https://avatars.githubusercontent.com/u/94872601?v=4",
+    city: "Porto Alegre"
+}
 
 function getUser() {
     axios.get(url)
@@ -9,3 +14,13 @@ function getUser() {
         .catch(error => console.log(error))
 }
 getUser()
+
+function addNewUser() {
+    axios.post(url, newUser)
+    .then(response => {
+        console.log(response.data)
+    })
+    .catch(error => console.log(error))
+}
+
+addNewUser()
